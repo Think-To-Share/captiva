9dcf73725e701f8ec9ca69a9891ab246b7415aa2
Rahul Dey <rahul900day@gmail.com>
2020-01-28 12:45:53 +0530

    some changes done

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -6,9 +6,26 @@ $(window).on("load", function() {
     $(window).resize(function() {
         maintainSliderImage();
     })
+
+    $(".homepage-page-slider .image-side .owl-carousel").owlCarousel({
+        loop: true,
+        margin:10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        nav: true,
+        navText: ['<i class="fas fa-arrow-circle-left fa-2x"></i>','<i class="fas fa-arrow-circle-right fa-2x"></i>'],
+        dots: false,
+        autoplayHoverPause:true,
+        responsive:{
+            1000:{
+                items: 1
+            }
+        }
+    });
 })
 
 function maintainSliderImage() {
     let contactBtnWidth = $("header.site-hader .main-header .menu-section ul li:last-child").width();
     $(".homepage-page-slider .image-side").css("padding-right", contactBtnWidth);
+    $(".service-page-slider .image-side").css("padding-right", contactBtnWidth);
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,4 @@
+import 'owl.carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
 import '../scss/app.scss';
 import './_homepage';
